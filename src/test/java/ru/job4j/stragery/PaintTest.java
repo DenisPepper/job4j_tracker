700ceb594fdbb5756161f2b4104cb29d4f7d8050
DenisPepper <denispepper>
2020-05-10 14:10:04 +0300

    9.1 Рефакторинг теста @Before @After [#259476]

--- src/test/java/ru/job4j/stragery/PaintTest.java
+++ src/test/java/ru/job4j/stragery/PaintTest.java
@@ -1,46 +1,53 @@
 package ru.job4j.stragery;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.StringJoiner;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import java.util.StringJoiner;
 
 public class PaintTest {
+    private final PrintStream stdout = System.out; // поле содержит дефолтный вывод в консоль.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream(); // буфер для результата.
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out; // получаем ссылку на стандартный вывод в консоль.
-        ByteArrayOutputStream out = new ByteArrayOutputStream(); // Создаем буфур для хранения вывода.
-        System.setOut(new PrintStream(out)); //Заменяем стандартный вывод на вывод в пямять для тестирования.
         new Paint().draw(new Square()); // выполняем действия пишушиее в консоль.
-
-        // проверяем результат вычисления
         StringJoiner pic = new StringJoiner(System.lineSeparator());
         pic.add("+++++++");
         pic.add("+     +");
         pic.add("+     +");
         pic.add("+++++++");
         pic.add("");
-        assertThat(new String(out.toByteArray()), is(pic.toString()));
+        assertThat(this.out.toString(), is(pic.toString()));
         System.setOut(stdout); // возвращаем обратно стандартный вывод в консоль.
     }
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out; // получаем ссылку на стандартный вывод в консоль.
-        ByteArrayOutputStream out = new ByteArrayOutputStream(); // Создаем буфур для хранения вывода.
-        System.setOut(new PrintStream(out)); //Заменяем стандартный вывод на вывод в пямять для тестирования.
         new Paint().draw(new Triangle()); // выполняем действия пишушиее в консоль.
-
-        // проверяем результат вычисления
         StringJoiner pic = new StringJoiner(System.lineSeparator());
         pic.add("    +  ");
         pic.add("  +   +");
         pic.add(" +     +");
         pic.add("+++++++++");
         pic.add("");
-        assertThat(new String(out.toByteArray()), is(pic.toString()));
+        assertThat(this.out.toString(), is(pic.toString()));
         System.setOut(stdout); // возвращаем обратно стандартный вывод в консоль.
     }
 }
