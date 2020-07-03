4d466a6fb4d1f714d4ac34c3fa9c7c5debb49da0
DenisPepper <denispepper>
2020-07-03 20:05:19 +0300

    0. Коллекции, ArrayList, List, Обобщения [#259463]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -4,18 +4,32 @@ import java.util.ArrayList;
 
 public class UsageArrayList {
 
-    public static void add(ArrayList arrayList, int index, String value) {
+    private static void addTo(ArrayList arrayList, int index, String value) {
         arrayList.add(index);
         arrayList.set(index, value);
     }
 
-    public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<String>();
-        add(names, 0, "Petr");
-        add(names, 1, "Ivan");
-        add(names, 2, "Stepan");
-        for (Object name:names) {
+    private static void add(ArrayList arrayList, String value) {
+        arrayList.add(value);
+    }
+
+    private static void print(ArrayList arrayList) {
+        for (Object name:arrayList) {
             System.out.println(name);
         }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<String>();
+        addTo(names, 0, "Petr");
+        addTo(names, 1, "Ivan");
+        addTo(names, 2, "Stepan");
+        print(names);
+
+        names.clear();
+        add(names, "Petr");
+        add(names, "Ivan");
+        add(names, "Stepan");
+        print(names);
     }
 }
