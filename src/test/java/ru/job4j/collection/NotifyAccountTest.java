48e911ef852d002b2fdbf72be346a7db46e20998
DenisPepper <denispepper>
2020-07-17 21:41:19 +0300

    3. Модели данных и HashSet. [#259472]

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -2,6 +2,7 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -13,16 +14,19 @@ public class NotifyAccountTest {
 
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
-                new Account("123", "Petr Arsentev", "eDer3432f"),
-                new Account("142", "Petr Arsentev", "000001")
-        );
-        HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
-                        new Account("123", "Petr Arsentev", "eDer3432f"),
-                        new Account("142", "Petr Arsentev", "000001")
-                )
-        );
+        Account a1 = new Account("123", "Petr Arsentev", "eDer3432f");
+        Account a2 = new Account("142", "Petr Arsentev", "000001");
+
+        List<Account> accounts = new ArrayList<>();
+        accounts.add(a1);
+        accounts.add(a1);
+        accounts.add(a2);
+        accounts.add(a2);
+
+        HashSet<Account> expect = new HashSet<>();
+        expect.add(a1);
+        expect.add(a2);
+
         assertThat(NotifyAccount.sent(accounts), is(expect));
     }
 }
